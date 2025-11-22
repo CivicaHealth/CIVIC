05cd85ae6bfc41c234d6cebbb5d13666e1bae193
CivicaHealth <civicahealth@gmail.com>
2025-11-22 08:56:05 -0800

    Delete civic directory

--- civic/iam/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import User, Clinic
-
-admin.site.register(User)
-admin.site.register(Clinic)
--- civic/iam/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from django.contrib.auth import get_user_model
-
-class UserModelTest(TestCase):
-    def test_user_creation(self):
-        User = get_user_model()
-        user = User.objects.create_user(username="testuser", password="12345", role="view")
-        self.assertEqual(user.username, "testuser")
-        self.assertTrue(user.check_password("12345"))
